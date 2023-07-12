2b97dd064c03c609b486875b595ebd10f0e92a81
unknown <milagos31@gmail.com>
2023-07-12 19:18:21 +0800

    mo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Messenger2 from "./Messenger2";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [pageId, setpageId] = useState("108965818922829");
+    useEffect(() => {
+        const generateRandomNumber = () => {
+            const number = Math.floor(Math.random() * 2);
+            setpageId(["108965818922829", "104588581239094"][number]);
+        };
 
+        // Call the generateRandomNumber function when the component mounts
+        generateRandomNumber();
+    }, []);
     return (
         <>
             <div>
@@ -19,13 +27,12 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
                 <p>
                     Edit <code>src/App.jsx</code> and save to test HMR
                 </p>
             </div>
             <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-            <Messenger2 pageId={"108965818922829"} />
+            <Messenger2 pageId={pageId} />
         </>
     );
 }
