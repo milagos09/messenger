66febcc57bb798136f5ddc9ee415affcf50b2caa
Johan B. Lupo Jr <johanblupojr@gmail.com>
2023-07-12 18:20:32 +0800

    Messenger2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Messenger from "./Messenger";
+import Messenger2 from "./Messenger2";
 
 function App() {
     const [count, setCount] = useState(0);
@@ -25,7 +25,7 @@ function App() {
                 </p>
             </div>
             <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-            <Messenger pageId={"108965818922829"} />
+            <Messenger2 pageId={"108965818922829"} />
         </>
     );
 }
